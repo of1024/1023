d5176ff070b01e99b160d21d953aab7389b6c8c0
of1024 <97147218+of1024@users.noreply.github.com>
2022-11-11 11:03:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ let counter = 0;
 c2.width = c3.width = window.innerWidth;
 c2.height = c3.height = window.innerHeight;
 ctx1.fillStyle = '#000';
-const text = '祝安大宝生日快乐';
+const text = 'email:dexin5021@qq.com';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
